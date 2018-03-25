35e439421802a7baf7c1f1a5688f4f062b22e470
Sharat Khanna <sharatkhanna@ssh.imad.hasura-app.io>
2018-03-25 12:34:02 +0530

    New Feature almost ready..tested so far, so good...

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+function newFeature() {
+	console.log('Big new feature with a lot of code')
+};
+
 function something() {
 	console.log('I\'m something');
 }
 console.log ('Hello World');
 something();
+newFeature();
